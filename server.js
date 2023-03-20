d5cc97e57606d1347cea9baf6d46410129999f7e
George-Laptop <glthomas526@gmail.com>
2023-03-20 11:03:34 -0400

    added function

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ let noteContents = [];
 let notesArray = [];
 
 // Set up Handlebars view engine
-app.engine("handlebars", exphbs.engine());
+app.engine(
+  "handlebars",
+  exphbs.engine({ defaultLayout: "main", layoutsDir: "views" })
+);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
@@ -20,24 +23,24 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("src"));
 
 app.get("/", (req, res) => {
-  res.render("layouts/main", { notesArray });
+  res.render("index", { notesArray });
 });
 
 app.post("/", (req, res) => {
   notesArray = database;
-  console.log(database);
-  res.render("layouts/main", { notesArray });
+  console.log("index");
+  res.render("index", { notesArray });
 });
 
 app.get("/new", (req, res) => {
   notesArray = database;
-  console.log(notesArray);
-  console.log(require("./views/layouts/new.handlebars"));
-  res.render("layouts/new.handlebars", { notesArray });
+  console.log("new");
+  res.render("new", { notesArray });
 });
 
 app.post("/new", (req, res) => {
-  notesArray = JSON.parse(database);
+  console.log(database);
+  notesArray = database;
   const noteTitle = req.body.noteTitle;
   const noteContent = req.body.noteContent;
   console.log(req.body.noteContent);
@@ -45,6 +48,8 @@ app.post("/new", (req, res) => {
   noteContents.push(noteContent);
   notesArray.push([noteTitle, noteContent]);
   console.log(notesArray.toString());
+  console.log("new");
+  console.log(notesArray);
   fs.writeFile("./db.json", JSON.stringify(notesArray), (err) => {});
 });
 
